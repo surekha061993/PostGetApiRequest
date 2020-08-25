8354480a1ec7b2ab83252423b20f4bf8fcafa4e3
user <user@DESKTOP-OUCT91H>
2020-08-25 00:30:19 -0800

    written findbyname test cases

--- SpringbootApis/src/test/java/com/example/demo/EmployeeTest.java
+++ SpringbootApis/src/test/java/com/example/demo/EmployeeTest.java
@@ -1,22 +1,39 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 import com.example.demo.dao.EmployeeDao;
 import com.example.demo.model.Employee;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace =Replace.NONE)
 public class EmployeeTest {
 
 	@Autowired
 	public EmployeeDao repo;
 	
 	@Test
-	public void createEmployee()
+	@Rollback(false)
+	public void testCreateEmployee()
 	{
 		Employee employee=new Employee(22,"surekha","pune","female","888888888");
-		repo.save(employee);
+		Employee emp=repo.save(employee);
+		assertNotNull(emp);
+	}
+	
+	@Test
+	public void testFindEmployeeByName()
+	{
+		String name="surekha";
+		Employee employee=repo.findByName(name);
+		assertThat(employee.getName()).isEqualTo(name);
 	}
 }
